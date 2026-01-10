a0afd4cf7780e2e9570ef74a6e00934eff6e896f
Akhilesh Mandawgane <akhileshmandawganeofficial@gmail.com>
2026-01-10 11:09:46 +0530

    auth-workflow implemented

---
+++ server/models/User.js
@@ -0,0 +1,41 @@
+// server/models/User.js
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ['student', 'company', 'admin'],
+        default: 'student',
+    },
+}, { timestamps: true });
+
+// Pre-save hook to hash password
+UserSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
